aebf4f9e7f397685fd9f8b48ee967ec63bac6c80
Леонид <lenya.zverevich@gmail.com>
2024-03-03 22:24:16 +0300

    fix design

--- app/(storepage)/cart/page.tsx
+++ app/(storepage)/cart/page.tsx
@@ -3,17 +3,17 @@ import Link from "next/link"
 
 export default function Page() {
   return (
-    <section className="font-poppins">
+    <section className="min-h-[70vh] font-poppins">
       <div className="container mx-auto p-5">
         <h1 className="mb-4 text-center text-2xl">Your cart items</h1>
         <Link
-          className="block text-center font-roboto text-[#56b280] hover:underline"
+          className="mb-5 block text-center font-roboto text-[#56b280] hover:underline"
           href={"/"}
         >
           Back to shopping
         </Link>
+        <CartItems />
       </div>
-      <CartItems />
     </section>
   )
 }
